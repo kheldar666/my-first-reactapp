f9cb50c32caeada4a6c291f2f4e83c613b27b845
Martin Papy <martin.papy@gmail.com>
2020-04-24 16:38:07 +0700

    Update

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Radium from 'radium';
+
+import './Person.css';
 
 const person  = (props) => {
     return (
-        <div>
-            <p>I am {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
 
-export default person;
+export default Radium(person);
